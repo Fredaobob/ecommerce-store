812b427af70a5a31d8ac3cf55ded9a4b99895c95
Fredaobob <lokgamer01@gmail.com>
2024-07-16 14:35:09 -0300

    development finished - store

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import Footer from "@/components/footer";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 
 
 const font = Urbanist({ subsets: ["latin"] });
@@ -20,6 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
+        <ToastProvider />
         <Navbar /> 
         {children}
         <Footer />
