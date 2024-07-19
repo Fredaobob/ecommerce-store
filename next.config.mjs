2225a0c18b7207f0b2b4c8745ccf684ea91c2704
Fredaobob <lokgamer01@gmail.com>
2024-07-19 13:47:03 -0300

    bug fixing attempt

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,28 @@ const nextConfig = {
     domains: [
       "res.cloudinary.com"
     ]
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
